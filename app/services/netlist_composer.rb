2eb14d9da813ce5f5ebee4a4b0c4517a2435d689
WitekL <witold.leicht@gmail.com>
2019-01-15 15:59:40 +0100

    Final changes. Implement Pi type network composer

--- app/services/netlist_composer.rb
+++ app/services/netlist_composer.rb
@@ -35,27 +35,29 @@ class NetlistComposer
   def compose_t(params)
     start_node = 0
     end_node = 1
-    mid_node = 0
+    mid_node = 1
     tmp_end_node = 0
     componenet_num = 1
-    netlist = []
+    netlist = ['SPICE netlist']
 
     params[:elements].each.with_index(1) do |branch, outer|
       branch[1].each.with_index(1) do |element, inner|
         start_node = end_node
         end_node += 1
 
-        mid_node = end_node if params[:elements][:left] == branch[1] && inner == branch[1].length
+        mid_node = end_node if branch[0] == 'left'  && inner == branch[1].length
 
-        if params[:elements][:middle] == branch[1]  && inner == branch[1].length
+        if branch[0] == 'middle'  && inner == branch[1].length
           tmp_end_node = end_node
           end_node = 0
         end
 
-        if params[:elements][:right] == branch[1] && inner == 1
+        if branch[0] == 'right' && inner == 1
           start_node = mid_node
           end_node = tmp_end_node + 1
         end
+
+        mid_node = end_node if branch[0] == 'right' && inner == branch[1].length
 
         string = "#{element[:type]}#{componenet_num} #{start_node} #{end_node} #{element[:value]}#{UNITS[element[:type]]}\n"
 
@@ -65,12 +67,55 @@ class NetlistComposer
       end
     end
 
-    [netlist, end_node]
+    [netlist, mid_node]
   end
 
   def compose_pi(params)
+    start_node = 0
+    end_node = 1
+    tmp_end_node = 2
+    second_source_node = 1
+    componenet_num = 1
+    netlist = ['SPICE netlist']
 
+    params[:elements].each.with_index(1) do |branch, outer|
+      branch[1].each.with_index(1) do |element, inner|
+        start_node = end_node
+        end_node += 1
+
+        # ostatni element left branch
+        if branch[0] == 'left' && inner == branch[1].length
+          tmp_end_node = end_node
+          end_node = 0
+        end
+
+        # pierwszy element middle branch
+        if branch[0] == 'middle' && inner == 1
+          start_node = 1
+          end_node = tmp_end_node
+        end
+
+        # drugie zrodlo
+        if branch[0] == 'middle' && inner == branch[1].length
+          second_source_node = end_node
+        elsif branch[0] == 'right' && inner == 1
+          second_source_node = start_node
+        end
+
+        if branch[0] == 'right' && inner == branch[1].length
+          end_node = 0
+        end
+
+        string = "#{element[:type]}#{componenet_num} #{start_node} #{end_node} #{element[:value]}#{ UNITS[element[:type]]}\n"
+
+        componenet_num += 1
+
+        netlist.push(string)
+      end
+    end
+
+    [netlist, second_source_node]
   end
 
-  UNITS =  { 'r' => '', 'c' => 'uF', 'l' => 'nH' }
+  UNITS =  { 'r' => '', 'c' => 'pF', 'l' => 'nH' }
 end
